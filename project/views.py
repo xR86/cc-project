9bad79451768247d756209ec3d3e064c8d3cb1bf
martafil <marta.dianna@gmail.com>
2017-05-08 15:43:40 +0300

    Added username in session

--- project/views.py
+++ project/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from models import login as _login
 from models import register as _register
 
+import datetime
+
 global_vars = {
     "app_name": "cc-project"
 }
@@ -17,6 +19,12 @@ def get_register_page(request):
 def get_login_page(request):
     return render(request, 'login.html', global_vars)
 
+def test(request):
+    value = request.COOKIES['username']
+    print value
+    return render(request, 'index.html', global_vars)
+
+   
 def register(request):
     username = request.POST["username"]
     passwd = request.POST["password"]
@@ -38,12 +46,17 @@ def register(request):
 def login(request):
     username = request.POST["username"]
     passwd = request.POST["password"]
-
+    print "#######################################"
     user = _login.get_user(username, passwd)
     if user:
         if user["status"] == "client":
-            return render(request, 'client.html', global_vars)
+            response = render(request, 'client.html', global_vars)
         else:
-            return render(request, 'provider.html', global_vars)
+            response = render(request, 'provider.html', global_vars)
+        max_age = 365 * 24 * 60 * 60  #one year
+        expires = datetime.datetime.strftime(datetime.datetime.utcnow() + datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+        response.set_cookie('username', username)#, max_age=max_age, expires=expires)
+        print username
+        return response
     else:
-        return render(request, 'error.html', global_vars)
+        return render(request, 'error.html', {"name": "caca"})
