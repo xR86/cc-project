94bcc35ec4e1a11ccaf0bca5e24128f2a7927e0d
tabby336 <ioana.bogdan25@gmail.com>
2017-05-09 18:57:28 +0300

    Typo.

--- project/models/mail.py
+++ project/models/mail.py
@@ -5,7 +5,7 @@ def send_register_mail(email_address):
     mail.send_mail(sender='ioana.bogdan25@gmail.com',
                    to=email_address,
                    subject="Your account has been approved",
-                   body="""Congrats, your accound has been created.""")
+                   body="""Congrats, your account has been created.""")
 
 
 def send_booking_confirmed_mail(email_address):
